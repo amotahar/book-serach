fc860b56082074b27726de35fc885896ede1468c
atuhin <atuhinbd@gmail.com>
2022-09-28 22:30:34 +0600

    dfd

---
+++ js/book.js
@@ -0,0 +1,9 @@
+
+const searchBook = () =>{
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText)
+    
+}
+// const bookShow = () => {}
+    
